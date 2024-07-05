1b710b0be260adb8a5e35bf8ec5bc0e05a29015a
icpnine <icpnine28@gmail.com>
2024-07-05 18:22:28 +0530

    react hot toast added

---
+++ src/views/Home/Home.js
@@ -0,0 +1,54 @@
+import "./Home.css"
+import React, { useEffect, useState } from 'react'
+import PlantCard from "../../compoenents/PlantCard/PlantCard"
+import axios from "axios"
+import toast, {Toaster} from "react-hot-toast"
+
+function Home() {
+  const [plants, setPlants] = useState([])
+
+  const loadPlants = async () => {
+    toast.loading("Loading plants...")
+    const response = await axios.get(`http://localhost:8000/plants`)
+
+    toast.dismiss()
+    toast.success("Plants loaded successfully")
+
+    setPlants(response.data.data)
+  }
+
+  useEffect(()=>{
+    loadPlants()
+  }, [])
+
+  return (
+    <div>
+      <h1>Plants</h1>
+
+      {
+        plants.map((plant, i)=>{
+          const {
+            _id, 
+            name, 
+            category, 
+            price, 
+            image, 
+            description
+          } = plant
+
+          return (<PlantCard 
+              key={i}
+              _id={_id} 
+              name={name} 
+              category={category} 
+              price={price} 
+              image={image} 
+              description={description}/>)
+        })
+      }
+      <Toaster />
+    </div>
+  )
+}
+
+export default Home
